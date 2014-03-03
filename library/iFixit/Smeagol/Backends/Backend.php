4c1b2615aeebf9351aa58f06f132d1e81b98aac9
Marc Zych <marczych@gmail.com>
2014-03-02 18:12:29 -0700

    Add javadoc comments to Backend

--- library/iFixit/Smeagol/Backends/Backend.php
+++ library/iFixit/Smeagol/Backends/Backend.php
@@ -10,11 +10,41 @@ use iFixit\Smeagol;
 abstract class Backend {
    const MISS = null;
 
+   /**
+    * Associates the value with the key in the cache. The value will expire
+    * after the specified expiration time in seconds.
+    *
+    * @return true on success, false on failure
+    */
    public abstract function set($key, $value, $expiration = 0);
+
+   /**
+    * Same as set except it does nothing if the key already exists.
+    *
+    * @return true on success, false if the key exists or the operation fails
+    */
    public abstract function add($key, $value, $expiration = 0);
+
+   /**
+    * Retrieves the value associated with the key.
+    *
+    * @return the value or null on failure or if it is not found
+    */
    public abstract function get($key);
+
+   /**
+    * Deletes the cache entry with the given key.
+    *
+    * @return true on success, false on failure
+    */
    public abstract function delete($key);
 
+   /**
+    * Wrapper around get and set that uses the provided callback to retrieve
+    * and populate the cache if the key is not found in the cache.
+    *
+    * @return the value
+    */
    public function getAndSet($key, callable $callback, $expiration = 0) {
       $value = $this->get($key);
 
