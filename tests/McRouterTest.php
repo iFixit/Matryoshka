5f69ee9c2b832048d073493eeb8dd40695033a97
Daryl Metzler <daryl@ifixit.com>
2018-06-08 11:03:41 -0700

    Make McrouterTest extend MemcachedTest
    This means we can gut everything except the `getBackend` method. The
    `setup` and `testExpiration` methods are satisfactory for testing.

--- tests/McRouterTest.php
+++ tests/McRouterTest.php
@@ -1,18 +1,10 @@
 <?php
 
-require_once 'AbstractBackendTest.php';
+require_once 'MemcachedTest.php';
 
 use iFixit\Matryoshka;
 
-class McRouterTest extends AbstractBackendTest {
-   protected function setUp() {
-      if (!Matryoshka\McRouter::isAvailable()) {
-         $this->markTestSkipped('Backend not available!');
-      }
-
-      return parent::setUp();
-   }
-
+class McRouterTest extends MemcachedTest {
    protected function getBackend() {
       $memcached = new Memcached();
       $memcached->addServer('localhost', 11211);
@@ -20,15 +12,5 @@ class McRouterTest extends AbstractBackendTest {
       $memcached->setOption(Memcached::OPT_TCP_NODELAY, true);
 
       return Matryoshka\McRouter::create($memcached);
-   }
-
-   public function testMcRouter() {
-      $backend = $this->getBackend();
-      list($key, $value) = $this->getRandomKeyValue();
-      $this->assertTrue($backend->set($key, $value, 1));
-      // Wait for it to expire.
-      sleep(2);
-
-      $this->assertNull($backend->get($key));
    }
 }
