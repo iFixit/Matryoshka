aff2096065f8f6851fe23da059397dde9cb29abb
Daniel Beardsley <daniel@ifixit.com>
2026-06-30 11:47:13 -0700

    Scope: Delete key when deleting scope
    Previously, to "delete" a scope, we immediately generated a new value
    and set() it overtop of the previous one.
    
    1. This isn't really needed. Sometimes we deleteScope() and don't
       immediately access it, so doing it eagerly was un-needed
    2. When using McRouter, delete() is guaranteed to be propagated
       while set() has a less strong guarantee
    
    Yes, this means that in some cases (hard to tell how common) delete is
    followed by a set() of the scope key shortly afterward and this change
    adds a delete where one wasn't present before.
    
    However, I think this is outweighed by point 1 above and point 2
    shows that this change is important.
    
    The code as it was meant that in our odd case with a bifurcated cache,
    we had to issue a delete() with every set() because deleteScope() only
    did a set(). There is a bunch of nuance here (see
    https://github.com/iFixit/ifixit/pull/62768).

--- library/iFixit/Matryoshka/Scope.php
+++ library/iFixit/Matryoshka/Scope.php
@@ -9,7 +9,7 @@ class Scope extends Prefix {
    private $scopePrefix;
 
    public function __construct(Backend $backend, $scopeName) {
-      // The prefix we pass along to the Prefix() constructor is never used, so 
+      // The prefix we pass along to the Prefix() constructor is never used, so
       // it doesn't matter.
       parent::__construct($backend, /* $prefix = */ null);
 
@@ -47,10 +47,12 @@ class Scope extends Prefix {
     * this scope.
     */
    public function deleteScope(): bool {
-      // Delete the scope by setting a new value for it.
-      $prefix = $this->getScopePrefix($reset = true);
+      // Explicitly delete the scope key from the backend to ensure that it is
+      // removed.
+      $this->backend->delete($this->getScopeKey());
+      $this->scopePrefix = null;
 
-      return $prefix !== self::MISS;
+      return true;
    }
 
    private function getScopeKey() {
